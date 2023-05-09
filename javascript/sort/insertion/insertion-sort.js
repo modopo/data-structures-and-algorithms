375c6af9868e2e4774b1ecaf168787c89cf7deb3
Jonathan Tsai <modopo@gmail.com>
2023-05-08 22:06:21 -0700

    completed insertion sort

---
+++ javascript/sort/insertion/insertion-sort.js
@@ -0,0 +1,28 @@
+'use strict'
+
+function insert(sorted, value) {
+  let i = 0;
+  while (value > sorted[i]) {
+    i++;
+  }
+  while (i < sorted.length) {
+    let temp = sorted[i];
+    sorted[i] = value;
+    value = temp;
+    i++;
+  }
+  sorted.push(value);
+}
+
+
+function insertionSort(input) {
+  let sorted = [];
+  sorted[0] = input[0];
+  for (let i = 1; i < input.length; i++) {
+    insert(sorted, input[i]);
+  }
+
+  return sorted;
+}
+
+module.exports = insertionSort;
